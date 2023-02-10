89e683d346d629e4d327f3542da38064da680a23
Kunniii <nguyenairport56@gmail.com>
2023-02-11 04:01:07 +0700

    attendance api

---
+++ index.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import express, { json, response } from "express";
+import BodyParser from "body-parser";
+import {
+  checkSession,
+  jsonifyHTMLData,
+  makeRequest,
+  all,
+  any,
+} from "./utils.js";
+
+const clog = console.log;
+const app = express();
+const PORT = process.env.PORT || 9090;
+
+app.use(BodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.post("/attendance", (req, res) => {
+  let sid = req.body.id || undefined;
+  let id = req.query.id;
+  let campus = req.query.campus;
+  let term = req.query.term;
+  let course = req.query.course;
+  if (!sid) {
+    res.status(400).json({ status: "NEED SESSION ID" });
+  } else {
+    if (!all([id, campus, term, course])) {
+      if (!any([id, campus, term, course])) {
+        makeRequest(sid, null).then((response) => {
+          if (checkSession(response.data)) {
+            res.json({ status: "OK", data: jsonifyHTMLData(response.data) });
+          } else {
+            res.status(400).json({ status: "LOGGED OUT" });
+          }
+        });
+      } else {
+        res.status(400).json({ status: "NEED MORE PARAMS" });
+      }
+    } else {
+      makeRequest(sid, { id, campus, term, course }).then((response) => {
+        if (checkSession(response.data)) {
+          res.json({ status: "OK", data: jsonifyHTMLData(response.data) });
+        } else {
+          res.status(400).json({ status: "LOGGED OUT" });
+        }
+      });
+    }
+  }
+});
+
+app.listen(PORT, (e) => {
+  if (e) {
+    clog(e.message);
+  } else {
+    clog(`OK! Listen on http://localhost:${PORT}`);
+  }
+});
---
+++ utils.js
@@ -0,0 +1,179 @@
+import { parse } from "node-html-parser";
+import HtmlTableToJson from "html-table-to-json";
+import axios from "axios";
+
+export async function makeRequest(sid, params) {
+  const baseUrl = "https://fap.fpt.edu.vn/Report/ViewAttendstudent.aspx";
+  const headers = {
+    "authority": "fap.fpt.edu.vn",
+    "accept-language": "en-GB,en;q=0.5",
+    "cache-control": "max-age=0",
+    "cookie": `ASP.NET_SessionId=${sid}; G_AUTHUSER_H=1; G_ENABLED_IDPS=google`,
+    "referer": "https://fap.fpt.edu.vn/Student.aspx",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "sec-gpc": "1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "CodeChoVui.dev",
+  };
+  if (!params) {
+    return axios.get(baseUrl, {
+      headers,
+    });
+  } else {
+    return axios.get(baseUrl, {
+      headers,
+      params,
+    });
+  }
+}
+
+export function all(arr) {
+  for (let i of arr) {
+    if (!i) return false;
+  }
+  return true;
+}
+
+export function any(arr) {
+  for (let i of arr) {
+    if (i) return true;
+  }
+  return false;
+}
+
+export function checkSession(html) {
+  let doc = parse(html);
+  let theForms = doc.getElementsByTagName("form");
+  for (let form of theForms) {
+    if (form.getAttribute("action") == "./Default.aspx") {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function jsonifyHTMLData(html) {
+  try {
+    let doc = parse(html);
+
+    let termsHTML = doc.getElementsByTagName("table")[2].innerHTML;
+    let coursesHTML = doc.getElementsByTagName("table")[3].innerHTML;
+    let reportsHTML = doc.getElementsByTagName("table")[4].innerHTML;
+
+    let termsData = extractDataFromTermsHTML(termsHTML);
+    let coursesData = extractDataFromCoursesHTML(coursesHTML);
+    let reportsData = extractDataFromReportsHTML(reportsHTML);
+
+    return {
+      terms: termsData,
+      courses: coursesData,
+      currentCourseReport: reportsData,
+    };
+  } catch (error) {
+    return {};
+  }
+}
+
+function getParamsFromHref(href) {
+  let params = {};
+  href = href.replaceAll("?", "");
+  let array = href.split("&");
+  for (let item of array) {
+    let key = item.split("=")[0];
+    let value = item.split("=")[1];
+    params[key] = value;
+  }
+  return params;
+}
+
+function extractDataFromReportsHTML(html) {
+  let doc = HtmlTableToJson.parse(`<table>${html}</table>`);
+  let reports = doc.results[0];
+  reports.pop();
+  for (let index in reports) {
+    let slot_time = reports[index].Slot;
+    let slot = slot_time.split("_")[0];
+    let time = slot_time.split("_")[1].replace(")", "").replace("(", "");
+    reports[index] = { ...reports[index], Slot: slot, Time: time };
+  }
+  return reports;
+}
+
+function extractDataFromCoursesHTML(html) {
+  let coursesData = [];
+  let doc = parse(html);
+  let rows = doc.getElementsByTagName("tr");
+  let courseName, url, selected, current, requestParams, queryString;
+  for (let row of rows) {
+    for (let child of row.childNodes) {
+      if (child.rawTagName) {
+        let el = child.childNodes[0];
+        if (el.rawTagName == "b") {
+          courseName = child
+            .getElementsByTagName("b")[0]
+            .text.replace(/\)\(.*\)/g, ")");
+          url = "None";
+          selected = true;
+          current = courseName;
+        } else {
+          let href = child.getElementsByTagName("a")[0].getAttribute("href");
+          courseName = child.getElementsByTagName("a")[0].text;
+          url = "https://fap.fpt.edu.vn/Report/ViewAttendstudent.aspx" + href;
+          selected = false;
+          requestParams = getParamsFromHref(href);
+          queryString = href;
+        }
+      }
+    }
+    coursesData.push({
+      name: courseName,
+      url,
+      selected,
+      requestParams,
+      queryString,
+    });
+  }
+  return { current: current, courses: coursesData };
+}
+
+function extractDataFromTermsHTML(html) {
+  let termsData = [];
+  let doc = parse(html);
+  // extract terms data
+  let rows = doc.getElementsByTagName("tr");
+  // tr
+  let termName, url, selected, current, requestParams, queryString;
+  for (let row of rows) {
+    // td
+    for (let child of row.childNodes) {
+      if (child.rawTagName) {
+        // <a></a> || <b></b>
+        let el = child.childNodes[0];
+        if (el.rawTagName == "b") {
+          termName = el.text;
+          url = "None";
+          selected = true;
+          current = termName;
+        } else {
+          let href = child.getElementsByTagName("a")[0].getAttribute("href");
+          selected = false;
+          termName = el.text;
+          url = "https://fap.fpt.edu.vn/Report/ViewAttendstudent.aspx" + href;
+          requestParams = getParamsFromHref(href);
+          queryString = href;
+        }
+      }
+    }
+    termsData.push({
+      name: termName,
+      url,
+      selected,
+      requestParams,
+      queryString,
+    });
+  }
+  return { current, terms: termsData };
+}
