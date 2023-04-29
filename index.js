851810488c13310cfccb6a250df0046ffe78565e
Kunniii <nguyenairport56@gmail.com>
2023-04-30 03:11:35 +0700

    added logger

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import BodyParser from "body-parser";
+import morgan from "morgan";
+import { createStream } from "rotating-file-stream";
 
 import { attendance } from "./routers/attendance.js";
 import { examSchedule } from "./routers/examSchedule.js";
@@ -10,9 +12,17 @@ import { terms } from "./routers/terms.js";
 const clog = console.log;
 const app = express();
 const PORT = process.env.PORT || 9090;
+const logStream = createStream("access.log", {
+  interval: "1d",
+  compress: "gzip",
+  teeToStdout: true,
+  path: "./logs/",
+});
 
 app.use(BodyParser.json());
 app.use(cors());
+app.use(morgan("combined", { stream: logStream }));
+// app.use(morgan("short"));
 
 app.get("/", (req, res) => {
   res.json({ status: "OK" });
@@ -27,6 +37,8 @@ app.listen(PORT, (e) => {
   if (e) {
     clog(e.message);
   } else {
-    clog(`OK! Listen on http://localhost:${PORT}`);
+    clog(
+      `Listening on port ${PORT}\nIf you are using Docker, the logs can be found at /app/logs/\n`
+    );
   }
 });
